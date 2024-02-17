3fd6be27a0217d052a34dedd2f7eaf94c4c52280
zeinamery <159799650+zeinamery@users.noreply.github.com>
2024-02-17 20:45:00 +0330

    Add files via upload

---
+++ tamrin 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    for (int i = 4; i > 0; --i) {
+        for (int j = 0; j < i; ++j) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }    
+        
+    return 0;    
+}            
