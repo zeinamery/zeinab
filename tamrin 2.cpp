f1333ed450b7033f9bc94f2a23723081b45d2681
zeinamery <159799650+zeinamery@users.noreply.github.com>
2024-02-17 21:27:15 +0330

    Add files via upload

---
+++ tamrin 2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+
+     string result = "";
+    result += 90 ;
+    result += 101 ;
+    result += 105 ;
+    result += 110 ;
+    result += 97 ;
+    result += 98 ;
+    result += 65 ;
+    result += 109 ;
+    result += 101 ;
+    result += 114 ;
+    result += 121 ; 
+   
+    
+    cout << result << endl;
+
+    return 0;
+}
